f20cd4837f2ecceb0e3c74d01c75223196217c1b
kamihirasama <kamihirasama@gmail.com>
2022-08-04 18:40:07 +0900

    update site 2022/08/04

--- src/partials/GlobalFooter.vue
+++ src/partials/GlobalFooter.vue
@@ -1,13 +1,29 @@
+<i18n>
+{
+  "ja": {
+    "pageTop": "ページの先頭へ戻る"
+  },
+  "en": {
+    "pageTop": "Back to the top of page"
+  }
+}
+</i18n>
+
 <template>
   <footer class="GlobalFooter">
+    
+
     <div class="GlobalFooter__admin">
       <div>{{ $developper[$i18n.locale] }}</div>
     </div>
+    
   </footer>
 </template>
 
 <script>
+
 export default {
   name: 'GlobalFooter',
+  
 }
 </script>
